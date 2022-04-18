a10e8233f1f26b4499e082cbc91fe497913ebaed
ALFONSO ROJO <alfonso.rojo1@vodafone.com>
2022-04-18 20:16:36 +0200

    Debugging

--- cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
+++ cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
@@ -1,9 +1,11 @@
 class AutoStore_HairCare_PO {
     addHairCareProductsToBasket() {
         data.productName.forEach(function(element) {
-            cy.addProductToBasket(element);
+            cy.addProductToBasket(element).then(() => {
+                // debugger
+            });
         })
-        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click();
+        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click().debug();
     }
 
 }
