ef73a2abeaf884332f281a41bfd61795e83bb79e
ALFONSO ROJO <alfonso.rojo1@vodafone.com>
2022-04-20 19:03:31 +0200

    Added custom config file feature

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -28,3 +28,25 @@ if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
     style.setAttribute('data-hide-command-log-request', '');
     app.document.head.appendChild(style);
 }
+
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile(file) {
+    const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+
+    if(!fs.existsSync(pathToConfigFile)){
+        console.log('No custom config file found.')
+        return {};
+    }
+  
+    return fs.readJson(pathToConfigFile)
+  }
+  
+  // plugins file
+  module.exports = (on, config) => {
+    // accept a configFile value or use development by default
+    const file = config.env.configFile
+  
+    return getConfigurationByFile(file)
+  }
