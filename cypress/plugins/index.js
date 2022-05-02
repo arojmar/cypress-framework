93ea32038bb911aa36898f18adc71b0b906ae361
Alfonso <arojmar@gmail.com>
2022-05-02 19:38:56 +0200

    Cucumber steps implementation

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,20 +17,26 @@ const fs = require('fs-extra')
 const path = require('path')
 
 function getConfigurationByFile(file) {
-    const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
 
-    if(!fs.existsSync(pathToConfigFile)){
-        console.log('No custom config file found.')
-        return {};
-    }
-  
-    return fs.readJson(pathToConfigFile)
+  if(!fs.existsSync(pathToConfigFile)){
+      console.log('No custom config file found.')
+      return {};
   }
-  
-  // plugins file
-  module.exports = (on, config) => {
-    // accept a configFile value or use development by default
-    const file = config.env.configFile
-  
-    return getConfigurationByFile(file)
-  }
+
+  return fs.readJson(pathToConfigFile)
+}
+
+// plugins file
+module.exports = (on, config) => {
+  // accept a configFile value or use development by default
+  const file = config.env.configFile
+
+  return getConfigurationByFile(file)
+}
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
